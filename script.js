2f08c6b699236b3fb211b61e362c2686cb4de3a0
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-12 05:39:37 +0800

    feat: Set the first image as the currently selected image by default

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ function makeImageSlider(slider) {
     // Access 'previous' and 'next' buttons.
     const previousButton = slider.querySelectorAll('#previous');
     const nextButton = slider.querySelectorAll('#next');
+
+    // Set the first image as the currently selected image by default.
+    let currentImage = images[0];
+    currentImage.classList.add('visible');
 }
