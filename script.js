e628cbe14b404617cd5680408470f8409b00461e
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-12 05:28:11 +0800

    feat: Access previous and next buttons

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function makeImageSlider(slider) {
     // Save all slider's images to an array.
     const images = [...slider.querySelectorAll('img')];
+
+    // Access 'previous' and 'next' buttons.
+    const previousButton = slider.querySelectorAll('#previous');
+    const nextButton = slider.querySelectorAll('#next');
 }
