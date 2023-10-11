1e4ae3e12971554d2904134047c03a0d5d8c7c10
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-12 05:56:27 +0800

    fix: Replace querySelectorAll with querySelector only since the buttons are single

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function makeImageSlider(slider) {
     const images = [...slider.querySelectorAll('img')];
 
     // Access 'previous' and 'next' buttons.
-    const previousButton = slider.querySelectorAll('#previous');
-    const nextButton = slider.querySelectorAll('#next');
+    const previousButton = slider.querySelector('#previous');
+    const nextButton = slider.querySelector('#next');
 
     // Set the first image as the currently selected image by default.
     let currentImage = images[0];
