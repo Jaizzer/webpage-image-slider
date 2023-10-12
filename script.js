3b2f69e7f1237d00b256ee267bc98eba20913944
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-13 07:02:44 +0800

    feat: Get the slider indicator

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function makeImageSlider(slider) {
     // Save all slider's images to an array.
     const images = [...slider.querySelectorAll('img')];
+
+    // Get the slider indicator.
+    const sliderIndicator = slider.querySelector('.slider-indicator');
 }
